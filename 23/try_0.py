bb5e70bf8fd5a1de204e7edaca104e12af07f7c5
b07902047 <b07902047@linux2.csie.ntu.edu.tw>
2019-04-19 19:49:56 +0800

    23-0 update try_0.py

--- 23/try_0.py
+++ 23/try_0.py
@@ -20,8 +20,8 @@ prediction_train_1 = model.predict(all_x)
 prediction_test_1 = model.predict(test_x)
 
 # stage 2
-model = xgb.XGBRegressor(learning_rate=0.01126, max_depth=20, subsample=0.75, n_jobs=32, tree_method='hist', n_estimators=2000)
-model.fit(train_x, train_y, eval_set=val, eval_metric='mae', early_stopping_rounds=12, xgb_model='m0.model')
+model = xgb.XGBRegressor(learning_rate=0.01126, max_depth=5, subsample=0.75, n_jobs=32, tree_method='hist', n_estimators=1000)
+model.fit(train_x, train_y, eval_set=val, eval_metric='mae', early_stopping_rounds=10, xgb_model='m0.model')
 model.save_model('m0.model')
 
 # stage 2 prediciton
@@ -29,15 +29,15 @@ prediction_train_2 = model.predict(all_x)
 prediction_test_2 = model.predict(test_x)
 
 with open('train_stage_1_0.txt', 'w') as f:
-    for i in range(len(prediction_train_1)):
+    for i in prediction_train_1:
         print(i, file=f)
 with open('train_stage_2_0.txt', 'w') as f:
-    for i in range(len(prediction_train_2)):
+    for i in prediction_train_2:
         print(i, file=f)
 with open('test_stage_1_0.txt', 'w') as f:
-    for i in range(len(prediction_test_1)):
+    for i in prediction_test_1:
         print(i, file=f)
 with open('test_stage_2_0.txt', 'w') as f:
-    for i in range(len(prediction_test_2)):
+    for i in prediction_test_2:
         print(i, file=f)
 

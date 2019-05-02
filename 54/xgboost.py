4f965fa504a8577143a14ab82708ec23235fbeb3
b07902139 <b07902139@linux12.csie.ntu.edu.tw>
2019-05-03 03:30:53 +0800

    54:xgboost

---
+++ 54/xgboost.py
@@ -0,0 +1,6 @@
+from pfinit import *
+init('xgboost')
+
+from xgboost import XGBRegressor
+model=XGBRegressor(learning_rate=0.1,max_depth=6,n_estimators=500,n_jobs=-1,subsample=0.75,tree_method='hist')
+run(models=model)

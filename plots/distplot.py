0fdbfd9ce4ab65b099c940ba4adff5e810377d14
b07902047 <b07902047@linux3.csie.ntu.edu.tw>
2019-05-21 19:39:24 +0800

    add distplot.pdf

--- plots/distplot.py
+++ plots/distplot.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 test_x = np.load('../../X_test.npz')['arr_0']
 train_x = np.load('../../X_train.npz')['arr_0']
@@ -11,11 +12,13 @@ train_num = train_x.shape[0]
 tmp_x = np.concatenate((train_x, test_x), axis=0)
 print(f'concatenated: {tmp_x.shape}')
 
-fig = plt.figure(figsize=(30, 10))
-for i in range(16):
-    if i % 100 == 0:
-        print(i)
-    plt.subplot(4, 4, (i + 1))
-    sns.distplot(tmp_x[:, i], norm_hist=False, kde=False)
-    plt.title(f'feature {i + 1}')
-fig.savefig(f'distplot.pdf', bbox_inches='tight')
+with PdfPages('distplot.pdf') as pdf:
+    for i in range(625):
+        plt.figure(figsize=(20, 10))
+        plt.subplots_adjust(wspace=None, hspace=0.3)
+        for j in range(16 * i, 16 * (i + 1)):
+            plt.subplot(4, 4, j % 16 + 1)
+            sns.distplot(tmp_x[:, j], norm_hist=False, kde=False)
+            plt.title(f'feature {j + 1}')
+        pdf.savefig()
+        plt.close()

7f62a9fd0722d85a6ae1af0742385509086c48b2
b07902139 <b07902139@meow2.csie.ntu.edu.tw>
2019-06-15 17:41:18 +0800

    nothing

--- utils/split.py
+++ utils/split.py
@@ -1,8 +1,8 @@
 from numpy import *
 random.seed(1126)
 a=random.permutation(47500)
-X=load('X_train_old.npz')['arr_0']
-Y=load('Y_train_old.npz')['arr_0']
+X=load('X_all.npz')['arr_0']
+Y=load('Y_all.npz')['arr_0']
 X_train,Y_train=X[a[2500:]],Y[a[2500:]]
 X_val,Y_val=X[a[:2500]],Y[a[:2500]]
 #print(X_train.shape,Y_train.shape,X_val.shape,Y_val.shape)
